ee321c3745a5a7e20a90a2beced82494a0192502
DarrenStamper <darrenstamper@protonmail.com>
2021-03-28 15:24:18 +0100

    Initial Bomberman files added

---
+++ js/bombermanClassic.js
@@ -0,0 +1,12 @@
+'use strict';
+
+import { loadNavbar, navbarDropdown } from "./global.js";
+
+window.onload = async function () {
+    
+    //load navbar
+    loadNavbar().then( () => {
+        document.getElementById("navbarIcon").addEventListener("click", navbarDropdown);
+        document.getElementById("index").className = "active";
+    });
+}
